fc00b7b367cb8918397ad2e2a111ca67e9046fcb
Mircea Sorin Sebastian <sorynsoo@gmail.com>
2016-12-11 17:22:19 +0200

    Added console based ui + directrly conversion from any base to any base

--- OptionalHomework/numerationBasesModule/__main__.py
+++ OptionalHomework/numerationBasesModule/__main__.py
@@ -4,11 +4,121 @@ from numerationBasesModule.classes.numerationBases import *
 
 numerationBasesObj = NumerationBases()
 
-v1 = numerationBasesObj.numberToVector(125.16)
-v3 = numerationBasesObj.rapidConversionToLowerBase(v1,16,4)
 
-print(v3["integer"])
-print(v3["fractional"])
+menu = [
+    "",
+    "---  MENU  ---",
+    "1. Add two numbers",
+    "2. Substract two numbers",
+    "3. Multiply two numbers",
+    "4. Divide two numbers",
+    "5. Get reminder from the division of two numbers",
+    "6. Rapid conversion (bases 2 - 4 - 16 or 4 - 16) - rational numbers: ",
+    "7. Convert to base 10 (successive divisions/multiplications) - rational numbers:",
+    "8. Convert from base 10 (successive divisions/multiplications) - rational numbers:",
+    "9. Convert directly from any base to any base (successive divisions/multiplications) - rational numbers:",
+    ""
+]
 
 
-print(numerationBasesObj.vectorToNumber(v1["fractional"]))
+numerationBasesObj.conversionToHigherBase(10111.1011, 2, 8)
+
+
+while True:
+    for crt in menu:
+        print(crt)
+
+    try:
+        cmd = input("Command: ")
+        if cmd == "1":
+            #ADDITION
+            a = numerationBasesObj.numberToVector(int(input("First number: ")))
+            b = numerationBasesObj.numberToVector(int(input("Second number: ")))
+            a = a["integer"]
+            b = b["integer"]
+            base = int(input("Base: "))
+            print(numerationBasesObj.vectorToNumber(numerationBasesObj.sum(a, b, base)))
+
+        if cmd == "2":
+            #SUBSTRACTION
+            a = numerationBasesObj.numberToVector(int(input("First number: ")))
+            b = numerationBasesObj.numberToVector(int(input("Second number: ")))
+            a = a["integer"]
+            b = b["integer"]
+            base = int(input("Base: "))
+            print(numerationBasesObj.vectorToNumber(numerationBasesObj.sub(a, b, base)))
+
+        if cmd == "3":
+            #MULTIPLICATION
+            a = numerationBasesObj.numberToVector(int(input("First number: ")))
+            b = int(input("Second number: "))
+            a = a["integer"]
+
+            base = int(input("Base: "))
+            print(numerationBasesObj.vectorToNumber(numerationBasesObj.prod(a, b, base)))
+
+        if cmd == "4":
+            #DIVISION
+            a = numerationBasesObj.numberToVector(int(input("First number: ")))
+            b = int(input("Second number: "))
+            a = a["integer"]
+
+            base = int(input("Base: "))
+            print(numerationBasesObj.vectorToNumber(numerationBasesObj.div(a, b, base)))
+
+        if cmd == "5":
+            #REMAINDER OF THE DIVISION
+            a = numerationBasesObj.numberToVector(int(input("First number: ")))
+            b = int(input("Second number: "))
+            a = a["integer"]
+
+            base = int(input("Base: "))
+            print(numerationBasesObj.divR(a, b, base))
+
+        if cmd == "6":
+            #RAPID CONVERSION
+            a = numerationBasesObj.numberToVector(float(input("Number: ")))
+
+            baseSrc = int(input("Source base: "))
+            baseDst = int(input("Destination base: "))
+
+            if(baseSrc < baseDst):
+                res = numerationBasesObj.rapidConversionToHigherBase(a, baseSrc, baseDst)
+            else:
+                res = numerationBasesObj.rapidConversionToLowerBase(a, baseSrc, baseDst)
+
+            print(str(numerationBasesObj.vectorToNumber(res["integer"])) + "." + str(numerationBasesObj.vectorToNumber(res["fractional"])))
+
+        if cmd == "7":
+            # CONVERT TO BASE 10
+            a = float(input("Number: "))
+            baseSrc = int(input("Source base: "))
+
+            res = numerationBasesObj.convertToBase10(a, baseSrc)
+
+            print(str(numerationBasesObj.vectorToNumber(res["integer"])) + "." + str(numerationBasesObj.vectorToNumber(res["fractional"])))
+
+        if cmd == "8":
+            # CONVERT FROM BASE 10
+            a = float(input("Number: "))
+            baseDst = int(input("Destination base: "))
+
+            res = numerationBasesObj.convertFromBase10(a, baseDst)
+
+            print(str(numerationBasesObj.vectorToNumber(res["integer"])) + "." + str(numerationBasesObj.vectorToNumber(res["fractional"])))
+
+        if cmd == "9":
+            # CONVERT FROM ANY BASE TO BASE
+            a = float(input("Number: "))
+            baseSrc = int(input("Source base: "))
+            baseDst = int(input("Destination base: "))
+
+            if baseSrc < baseDst:
+                res = numerationBasesObj.conversionToHigherBase(a, baseSrc, baseDst)
+            else:
+                res = numerationBasesObj.conversionToLowerBase(a, baseSrc, baseDst)
+
+
+            print(str(numerationBasesObj.vectorToNumber(res["integer"])) + "." + str(numerationBasesObj.vectorToNumber(res["fractional"])))
+    except ValueError as e:
+        print("Invalid value !")
--- OptionalHomework/numerationBasesModule/classes/numerationBases.py
+++ OptionalHomework/numerationBasesModule/classes/numerationBases.py
@@ -81,6 +81,22 @@ class NumerationBases():
 
         return result
 
+    def prodP(self, a, nr, base):
+        '''
+        :param a: vector
+        :param nr: a number with 1 digit in specified base
+        :param base: numeration base
+        :return: return the extra digit
+        '''
+        result = []
+        t = 0
+
+        for i in range(len(a)):
+            newRes = a[i] * nr + t
+            t = newRes // base
+
+        return t
+
     def div(self, a, nr, base):
         '''
         :param a: vector
@@ -103,6 +119,21 @@ class NumerationBases():
 
         return result[::-1]
 
+    def divR(self, a, nr, base):
+        '''
+        :param a: vector
+        :param nr: a number with 1 digit in specified base
+        :param base: numeration base
+        :return: the rest of the division
+        '''
+        t = 0
+
+        for i in range(len(a)-1, -1, -1):
+            t = t * base + a[i]
+            t = t % nr
+
+        return t
+
     def numberToVector(self, nr):
         '''
         :param nr: a number in any base
@@ -119,15 +150,12 @@ class NumerationBases():
             convertedIntegerNumber.append(integer % 10)
             integer = integer // 10
 
-        precision = 4
-        i = 0
-        while fractional != 0.0 and i < precision:
-            fractional = fractional * 10
-            convertedFractionalNumber.append(self.getInteger(fractional))
-            fractional = round(fractional - self.getInteger(fractional), 4)
-            i = i + 1
+        while fractional != 0:
+            convertedFractionalNumber.append(fractional % 10)
+            fractional = fractional // 10
 
-        return {"integer": convertedIntegerNumber, "fractional": convertedFractionalNumber[::-1]}
+
+        return {"integer": convertedIntegerNumber, "fractional": convertedFractionalNumber}
 
     def vectorToNumber(self, a):
         '''
@@ -143,11 +171,55 @@ class NumerationBases():
 
         return nr
 
+    def vectorToSubunitaryNumber(self, a):
+        '''
+        :param a: a vector containing a subunitary number
+        :return: the subunitary number in float type
+        '''
+        p = 10
+        res = 0
+        counter = 0
+        for crt in range(len(a) - 1, -1, -1):
+            res = res + int(a[crt]) / p
+            p *= 10
+            counter += 1
+            if counter >= 4:
+                break
+
+        return res
+
     def getInteger(self, nr):
         return int(nr)
 
     def getFractional(self, nr):
-        return round(nr - self.getInteger(nr), 4)
+        if nr == int(nr):
+            return 0
+
+        splitNr = str(nr).split('.')
+        fractional = int(splitNr[1])
+
+        return fractional
+
+    def getFractionalString(self, nr):
+        if nr == int(nr):
+            return 0
+
+        splitNr = str(nr).split('.')
+        fractional = splitNr[1]
+
+        return fractional
+
+    def removeInteger(self, nr):
+        nr = str(nr).split('.')
+        nr = nr[1]
+
+        p = 10
+        res = 0
+        for crt in nr:
+            res = res + int(crt) / p
+            p *= 10
+
+        return res
 
     def convertFromBase10(self, nr, baseDst):
         '''
@@ -159,7 +231,7 @@ class NumerationBases():
         convertedFractionalNumber = []
 
         integer = self.getInteger(nr)
-        fractional = self.getFractional(nr)
+        fractional = self.removeInteger(nr)
 
         #Converting the integer part
         while integer != 0:
@@ -172,7 +244,7 @@ class NumerationBases():
         while fractional != 0.0 and crtDigits < maxDigits:
             newRes = fractional * baseDst
             convertedFractionalNumber.append(self.getInteger(newRes))
-            fractional = self.getFractional(newRes)
+            fractional = self.removeInteger(newRes)
             crtDigits = crtDigits + 1
 
 
@@ -230,6 +302,15 @@ class NumerationBases():
         :return: a dictionary containing two list (one with fractional and one with integer part)
         '''
 
+        if baseSrc == 16 and baseDst != 4 and baseDst != 2:
+            raise RuntimeError("Combination of bases is not corect. Must be 16 -> 4 or 16 -> 2")
+
+        if baseSrc == 8 and baseDst != 2:
+            raise RuntimeError("Combination of bases is not corect. Must be 8 -> 2")
+
+        if baseSrc == 4 and baseDst != 2:
+            raise RuntimeError("Combination of bases is not corect. Must be 4 -> 2")
+
         convertedIntegerNumber = []
         convertedFractionalNumber = []
 
@@ -243,20 +324,20 @@ class NumerationBases():
                 binaryDigits.append(0)
 
             for j in range(nrOfRepeats):
-                convertedIntegerNumber.append(binaryDigits[j])
+                convertedIntegerNumber.insert(0, binaryDigits[j])
 
         #Converting the fractional part
-        for i in range(len(a["fractional"])):
+        for i in range(len(a["fractional"])-1, -1, -1):
             binaryDigits = self.convertFromBase10(a["fractional"][i], baseDst)
             binaryDigits = binaryDigits["integer"]
             for j in range(nrOfRepeats - len(binaryDigits)):
                 binaryDigits.append(0)
-
+            binaryDigits = binaryDigits[::-1]
             for j in range(nrOfRepeats):
                 convertedFractionalNumber.append(binaryDigits[j])
 
 
-        return {"integer": convertedIntegerNumber, "fractional": convertedFractionalNumber[::-1]}
+        return {"integer": convertedIntegerNumber[::-1], "fractional": convertedFractionalNumber[::-1]}
 
     def rapidConversionToHigherBase(self, a, baseSrc, baseDst):
         '''
@@ -265,10 +346,160 @@ class NumerationBases():
         :param baseDst: the destination base
         :return: a dictionary containing two list (one with fractional and one with integer part)
         '''
-        pass
 
-    def conversionToLowerBase(self, a, baseSrc, baseDst):
-        pass
+        if baseSrc == 2 and baseDst != 4 and  baseDst != 8 and baseDst != 16:
+            raise RuntimeError("Combination of bases is not corect. Must be 2 -> 4 or 2 -> 8 or 2 -> 16")
 
-    def conversionToHigherBase(self, a, baseSrc, baseDst):
-        pass
+        if baseSrc == 4 and baseDst != 16:
+            raise RuntimeError("Combination of bases is not corect. Must be 4 -> 16")
+
+
+        convertedIntegerNumber = []
+        convertedFractionalNumber = []
+
+        nrOfRepeats = int(math.log(baseDst, baseSrc))
+
+        #Converting the integer part
+        i = 0
+        while i < len(a["integer"]):
+            binaryDigits = a["integer"][i:i+nrOfRepeats]
+
+            #binaryDigits = binaryDigits[::-1]
+            nrBinaryDigits = self.vectorToNumber(binaryDigits) # Convert it to a vector
+            nrBinaryDigits = self.convertToBase10(nrBinaryDigits, 2) # Convert the vector in base 10
+            nrBinaryDigits = self.vectorToNumber(nrBinaryDigits["integer"]) # Convert the vector in base 10
+            convertedIntegerNumber.append(nrBinaryDigits)
+
+            i = i + nrOfRepeats
+
+
+        i = 0
+        while i < len(a["fractional"]):
+            binaryDigits = a["fractional"][i:i+nrOfRepeats]
+
+            for j in range(nrOfRepeats - len(binaryDigits)):
+                binaryDigits.append(0)
+
+            binaryDigits = binaryDigits[::-1]
+            nrBinaryDigits = self.vectorToNumber(binaryDigits) # Convert it to a vector
+            nrBinaryDigits = self.convertToBase10(nrBinaryDigits, 2) # Convert the vector in base 10
+            nrBinaryDigits = self.vectorToNumber(nrBinaryDigits["integer"]) # Convert the vector in base 10
+            convertedFractionalNumber.append(nrBinaryDigits)
+
+            i = i + nrOfRepeats
+
+        return {"integer": convertedIntegerNumber[::-1], "fractional": convertedFractionalNumber[::-1]}
+
+    def conversionToLowerBase(self, nr, baseSrc, baseDst):
+        '''
+        :param nr: number to be converted
+        :param base:  destionation base
+        :return: a dictionary containing two list (one with fractional and one with integer part)
+        '''
+        convertedIntegerNumber = []
+        convertedFractionalNumber = []
+
+        a = self.numberToVector(nr)
+        integer = a["integer"]
+        fractional = a["fractional"]
+
+        #Converting the integer part
+        while len(integer):
+            rest = self.divR(integer, baseDst, baseSrc)
+            quotent = self.div(integer, baseDst, baseSrc)
+
+            convertedIntegerNumber.append(rest)
+            integer = quotent
+
+        #Converting the fractional part
+        maxDigits = 4
+        crtDigits = 0
+        while len(fractional)  and crtDigits < maxDigits:
+            integerDigit = self.prodP(fractional, baseDst, baseSrc)
+            newFractional = self.prod(fractional, baseDst, baseSrc)
+
+            convertedFractionalNumber.append(integerDigit)
+
+            if(len(newFractional) > len(fractional)):
+                newFractional.pop(len(newFractional)-1)
+
+            fractional = newFractional
+            crtDigits = crtDigits + 1
+
+
+        return {"integer": convertedIntegerNumber, "fractional": convertedFractionalNumber[::-1]}
+
+    def conversionToHigherBase(self, nr, baseSrc, baseDst):
+         '''
+         Substitution Method
+         :param nr: number to be converted
+         :param baseSrc: the destination base
+         :return: a dictionary containing two list (one with fractional and one with integer part)
+         '''
+
+         convertedIntegerNumber = []
+         convertedFractionalNumber = []
+
+         a = self.numberToVector(nr)
+         integer = a["integer"]
+         fractional = a["fractional"]
+
+         #Converting the integer part
+         power = 1
+
+         for i in range(len(integer)):
+             crtPower = self.numberToVector(power)
+             crtPower = crtPower["integer"]
+             crtProd = self.prod(crtPower, integer[i], baseDst)
+
+             convertedIntegerNumber = self.sum(convertedIntegerNumber, crtProd, baseDst)
+
+             power = self.prod(crtPower, baseSrc, baseDst)
+             power = self.vectorToNumber(power)
+
+
+         #Converting the fractional part
+         powerHeins = 1 / baseSrc
+         power = powerHeins
+         power = self.convertFromBase10(power, baseDst)
+         power = self.vectorToSubunitaryNumber(power["fractional"])
+
+         for i in range(len(fractional)-1, -1, -1):
+             crtFractional = self.numberToVector(fractional[i])
+             crtFractional = crtFractional["integer"]
+
+
+             if len(crtFractional) != 0:
+                crtProd = self.prod(crtFractional, power, baseDst)
+                crtProd = self.convertRationalToInteger(crtProd[0], 4)
+
+                convertedFractionalNumber = self.sum(convertedFractionalNumber, crtProd, baseDst)
+
+
+
+             powerHeins = powerHeins / baseSrc
+             power = powerHeins
+             power = self.convertFromBase10(power, baseDst)
+             power = self.vectorToSubunitaryNumber(power["fractional"])
+
+
+
+
+         return {"integer": convertedIntegerNumber, "fractional": convertedFractionalNumber}
+
+    def convertRationalToInteger(self,a, precision):
+        '''
+        :param a: a rational number
+        :return: the fractionary part as an integer
+        '''
+
+        res = []
+
+        nr = self.getFractionalString(a)
+        for crt in nr:
+            res.append(int(crt))
+
+        for i in range(precision-len(res)):
+            res.append(0)
+
+        return res[::-1]
